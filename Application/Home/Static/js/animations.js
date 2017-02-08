9d00310881e7397e123a8b33f48123ce03bc89cd
sky3936 <59262424@qq.com>
2017-02-08 12:40:55 +0800

    首页持续完善...

--- Application/Home/Static/js/animations.js
+++ Application/Home/Static/js/animations.js
@@ -10,15 +10,19 @@
 			if (imagePos < topOfWindow+700) {
 				if($(this).hasClass('muu')){
 					$(this).addClass("fadeInDown");
+					$(this).removeClass("animate-box");
 				}
 				if($(this).hasClass('deve')){
 					$(this).addClass("fadeInLeft");
+					$(this).removeClass("animate-box");
 				}
 				if($(this).hasClass('store')){
 					$(this).addClass("fadeInRight");
+					$(this).removeClass("animate-box");
 				}
 				if($(this).hasClass('need')){
 					$(this).addClass("fadeInUp");
+					$(this).removeClass("animate-box");
 				}
 			}
 		});	
@@ -31,14 +35,24 @@
 				$(this).addClass("bounceIn");
 			}
 		});	
+
+		$('.scheme .ts-d_ul li').each(function(){
+			var imagePos = $(this).offset().top;
+			
+			var topOfWindow = $(window).scrollTop();
+				if (imagePos < topOfWindow+500) {
+					$(this).addClass("flipInY");
+					$(this).removeClass("animate-box");
+				}
+		});	
         
         $('.developer .fh5co-box').each(function(){
-		var imagePos = $(this).offset().top;
-		
-		var topOfWindow = $(window).scrollTop();
-			if (imagePos < topOfWindow+500) {
-				$(this).addClass("fadeInUp");
-			}
+			var imagePos = $(this).offset().top;
+			
+			var topOfWindow = $(window).scrollTop();
+				if (imagePos < topOfWindow+500) {
+					$(this).addClass("fadeInUp");
+				}
 		});	
 				
 	});
