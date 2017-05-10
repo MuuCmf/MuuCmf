fedc80efec42c9c8f885f919c66f2ededc0e9c2d
严蒙 <59262424@qq.com>
2017-05-10 12:04:25 +0800

    后端应用商店路径错误

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -57,7 +57,7 @@ return array(
         '__CSS__' => __ROOT__ . '/Application/' . MODULE_NAME . '/Static/css',
         '__JS__' => __ROOT__ . '/Application/' . MODULE_NAME . '/Static/js',
         '__ZUI__' => __ROOT__ . '/Application/' . MODULE_NAME . '/Static/zui',
-        '__CLOUD__' => 'http://muucmf.hoomuu.cn',
+        '__CLOUD__' => 'https://www.hoomuu.cn',
     ),
     '__CLOUD__'=>'http://www.hoomuu.cn',
     'UPDATE_PATH'=>'./Data/Update/',
