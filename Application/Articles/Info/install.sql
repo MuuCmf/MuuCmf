8ece94b136068317e5f9d4fb438254c94c571813
Yan Meng <59262424@qq.com>
2018-01-05 10:14:05 +0800

    增加个文章模块安装时的数据库脚本

--- Application/Articles/Info/install.sql
+++ Application/Articles/Info/install.sql
@@ -2,10 +2,10 @@
 -- 表结构 `muucmf_articles`
 -- -----------------------------
 CREATE TABLE IF NOT EXISTS `muucmf_articles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
   `uid` int(11) NOT NULL,
   `title` varchar(50) NOT NULL COMMENT '标题',
-  `keywords` varchar(255) NOT NULL COMMENT '关键字',
+  `keywords` varchar(255) NOT NULL COMMENT '关键字，多个用,分割',
   `description` varchar(200) NOT NULL COMMENT '描述',
   `category` int(11) NOT NULL COMMENT '分类',
   `status` tinyint(2) NOT NULL COMMENT '状态',
@@ -20,14 +20,14 @@ CREATE TABLE IF NOT EXISTS `muucmf_articles` (
   `create_time` int(11) NOT NULL,
   `update_time` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='文章';
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COMMENT='文章';
 
 
 -- -----------------------------
 -- 表结构 `muucmf_articles_category`
 -- -----------------------------
 CREATE TABLE IF NOT EXISTS `muucmf_articles_category` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
   `title` varchar(20) NOT NULL,
   `pid` int(11) NOT NULL,
   `can_post` tinyint(4) NOT NULL COMMENT '前台可投稿',
@@ -35,20 +35,24 @@ CREATE TABLE IF NOT EXISTS `muucmf_articles_category` (
   `sort` tinyint(4) NOT NULL,
   `status` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='文章分类';
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='文章分类';
 
 
 -- -----------------------------
 -- 表结构 `muucmf_articles_detail`
 -- -----------------------------
 CREATE TABLE IF NOT EXISTS `muucmf_articles_detail` (
-  `news_id` int(11) NOT NULL,
+  `articles_id` int(11) NOT NULL,
   `content` text NOT NULL COMMENT '内容',
-  `template` varchar(50) NOT NULL COMMENT '模板',
-  PRIMARY KEY (`news_id`)
+  `template` varchar(50) NOT NULL COMMENT '模板'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='文章详情';
 
 -- -----------------------------
 -- 表内记录 `muucmf_articles_category`
 -- -----------------------------
-INSERT INTO `muucmf_articles_category` VALUES ('1', '默认分类', '0', '1', '1', '1', '1');
+INSERT INTO `muucmf_articles_category` VALUES ('1', '早期创业', '0', '1', '1', '1', '1');
+INSERT INTO `muucmf_articles_category` VALUES ('2', '独角兽', '0', '1', '1', '3', '1');
+INSERT INTO `muucmf_articles_category` VALUES ('3', '投融资', '0', '1', '1', '2', '1');
+INSERT INTO `muucmf_articles_category` VALUES ('4', '火木动态', '0', '1', '1', '0', '1');
+INSERT INTO `muucmf_articles_category` VALUES ('5', '锐观察', '0', '1', '1', '4', '1');
+INSERT INTO `muucmf_articles_category` VALUES ('6', '技术创新', '0', '1', '1', '5', '1');
