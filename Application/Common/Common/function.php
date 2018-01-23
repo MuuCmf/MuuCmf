c4b5f7bb4d1f6aae273805347929f950d760cb53
Yan Meng <59262424@qq.com>
2018-01-24 00:02:13 +0800

    【新增】获取当前完整URL与判断网址是否包含参数的公共函数

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -1632,3 +1632,30 @@ function muu_now_theme(){
         }
     return $now_theme;
 }
+/**
+ * 获取当前完整URL
+ * @return [type] [description]
+ */
+function get_url() {
+    $sys_protocal = isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443' ? 'https://' : 'http://';
+    $php_self = $_SERVER['PHP_SELF'] ? $_SERVER['PHP_SELF'] : $_SERVER['SCRIPT_NAME'];
+    $path_info = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
+    $relate_url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : $php_self.(isset($_SERVER['QUERY_STRING']) ? '?'.$_SERVER['QUERY_STRING'] : $path_info);
+    return $sys_protocal.(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '').$relate_url;
+}
+/**
+ * 判断网址是否包含参数,有参数返回后缀&，无返回后缀？
+ * @param  [type] $url [description]
+ * @return [type]      [description]
+ */
+function url_query($url){
+    $array = parse_url($url);
+    if(!isset($array['query'])){
+        $url = $url.'?';
+    }else{
+        $url = $url.'&';
+    }
+    return $url;
+}
+
+?>
